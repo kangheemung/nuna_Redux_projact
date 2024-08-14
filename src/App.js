d02b1c49d0ece4ee4c0016e31d829da16087b2d0
kurotamashi30 <kurotamashi30@gmail.com>
2024-08-14 13:08:12 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import Box from "./component/Box";
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,33 +7,38 @@ import { useDispatch, useSelector } from 'react-redux';
 //버튼을 누르면 +1씩 증가된 숫자 출력
 
 function App() {
-  const count = useSelector(state=>state.count);
+  const count = useSelector(state => state.count);
+  const id = useSelector(state => state.id);
+  const password = useSelector(state => state.password);
   const dispatch = useDispatch();
 
   const increaseNum= ()=>{
-    dispatch({type:"INCREMENT",payload:{num:5} })
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
     //type=액션의 이름(アクションの名前)
 
   };
+  const decrease = () => {
+    dispatch({ type: "DECREASE" });
+  };
+  
   const login=()=>{
-    dispatch({type:"LOGIN",payload:{id:"nuna" ,password: "123"}});
-    case "LOGIN":
-      return{
-        ...state,
-        id:acrion.payload.id,
-        password: action.payload}
-    default:
-      return{...state};
+    dispatch({ type: "LOGIN", payload: { id: "nuna", password: "123" } });
+
     };
 
-  return (
-    <div className="App">
-      <button onClick={increaseNum}> 플러스+1추가</button>
-      출력값: {count}
-      <burron onClick={login}>Login</burron>
-      <Box/>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>
+          {id}, {password}
+        </h1>
+        <button onClick={increaseNum}>플러스+1추가</button>
+        출력값: {count}
+        <button onClick={login}>Login</button>
+       < button onClick={decrease}> 감소 </ button>
+       감소출력값: {count}
+        <Box />
+      </div>
+    );
 }
 
 export default App;
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -13,8 +13,18 @@ function reducer(state = initialState, action){
     switch(action.type){
         case "INCREMENT":
             return {...state, count: state.count + action.payload.num };
-        default:
-            return {...state};
+            case "DECREASE":
+                return { ...state, count: state.count - action.payload.num };
+        case "LOGIN":
+            return {
+                ...state,
+                id: action.payload.id,
+                password: action.payload.password,
+                };
+
+            default:
+            return{...state};
+    
     }
 }
 
