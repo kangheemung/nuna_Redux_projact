e8542a98c40283ad1b9d28895fac8c1a67cdb5f9
kurotamashi30 <kurotamashi30@gmail.com>
2024-08-18 00:00:51 +0900

    done

--- src/App.js
+++ src/App.js
@@ -25,15 +25,17 @@ function App() {
 
 
   return (
-    <div className="App">
+    
+  <div className="App">
+    <div className="body_container">
       <div className='name'>name: {id}</div>
   
       <input
         id="name-input"
-        placeholder='名前を入力してください'
+        placeholder='이름을 입력해주세요'
       />
       <button onClick={handleLogin}>
-        Update Name
+        내 캐릭터의 이름은?
       </button>
   
       <h1>Game Character Status</h1>
@@ -44,36 +46,37 @@ function App() {
       {status['luck'] > 19 && (
         <div className="image-container">
           <img className='img' src='https://image.fmkorea.com/files/attach/new4/20240412/6913252080_1135415169_36607c0bb8bff08e1cc3ed291e2f7895.jpeg' alt='' />
-          <div>koko</div>
+          <div> 마법사</div>
         </div>
       )}
-      {status['speed'] > 10 && (
+      {status['speed'] > 20 && (
         <div className="image-container">
           <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJVNKa6a0IVLMT6xj_NPKvfyw-EB7Fjc5xSZnIY4B71C8EA7SDn1NbDBOoorHPWAzoNgE&usqp=CAU' alt='' />
-          <div>koko</div>
+          <div>어둠의 자식
+          </div>
         </div>
       )}
-       {status['power'] > 10 && (
+       {status['power'] > 18 && (
         <div className="image-container">
           <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBa1FxORcUGeTEgA5KwvNoIXD4KppuCxRDnQ&s' alt='' />
-          <div>koko</div>
+          <div>용사</div>
         </div>
       )}
        {status['defense'] > 48 && (
         <div className="image-container">
           <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr7ITD7o_g5O7JbqWRfs7pjHCmHTp2ju0Upw&s' alt='' />
-          <div>koko</div>
+          <div>골렘</div>
         </div>
       )}
    {!(
         status['luck'] > 19 ||
-        status['speed'] > 10 ||
-        status['power'] > 10 ||
+        status['speed'] > 20 ||
+        status['power'] > 18 ||
         status['defense'] > 48
       ) && (
         <div className="image-container">
           <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOkOwhHWNKJYNC60_ErXLGfIHfv-9GjwmRyg&s' alt='' />
-          <div>koko</div>
+          <div>일반인</div>
         </div>
       )}
       <div className="status-container" style={{ display: 'flex', flexDirection: 'row' }}>
@@ -92,6 +95,7 @@ function App() {
         ))}
       </div>
     </div>
+  </div>
   );
 
 }
